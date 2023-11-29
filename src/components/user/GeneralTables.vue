3f7e9b9ab524fe00c79f2484f4d9d86eec7c0908
廖保发 <1046650845@qq.com>
2023-11-30 05:57:42 +0800

    封装table，方便后续使用

---
+++ src/components/user/GeneralTables.vue
@@ -0,0 +1,41 @@
+<!-- 封装表格多用 -->
+<script setup lang="ts">
+// 父传子
+defineProps({
+    tableData:Array
+})
+
+// 编辑
+const handleEdit=(row:any)=>{
+  console.log(row);
+  
+}
+
+// 删除
+const handleDelete=(id:any)=>{
+  console.log(id);
+}
+</script>
+
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column type="index" label="序号" width="200px"  align="center"></el-table-column>
+    <slot name="tableColumns"></slot>
+    <el-table-column label="编辑" align="center">
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.row)"
+          >编辑</el-button
+        >
+        <el-button
+          size="small"
+          type="danger"
+          @click="handleDelete(scope.row.id)"
+          >删除</el-button
+        >
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<style scoped lang="scss">
+</style>
--- src/views/user/Role.vue
+++ src/views/user/Role.vue
@@ -2,8 +2,9 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { getRoleList } from '../../api/role'
+import GeneralTables from '../../components/user/GeneralTables.vue'
 // 获取角色列表
-const roleList = ref<any>([])
+const roleList = ref([])
 const getRoleListFun = async () => {
   const res = await getRoleList()
   
@@ -12,44 +13,27 @@ const getRoleListFun = async () => {
 }
 getRoleListFun()
 
-// 编辑
-const handleEdit=(index:any,row:any)=>{
-  console.log(index,row);
-  
-}
-
-// 删除
-const handleDelete=(index:any,row:any)=>{
-  console.log(index,row);
-}
 </script>
 
 <template>
-  <el-table :data="roleList" style="width: 100%">
-    <el-table-column prop="roleId" label="角色" width="180"></el-table-column>
-    <el-table-column label="角色名称" width="180">
+  <!-- 添加按钮 -->
+  <div>
+    <el-button type="primary" plain style="margin-bottom: 20px;">添加</el-button>
+  </div>
+  <GeneralTables :tableData="roleList">
+    <template #tableColumns>
+      <el-table-column label="角色" align="center">
       <template #default="scope">
-        <el-popover effect="light" trigger="hover" placement="top" width="auto">
-          <template #reference>
-            <el-tag>{{ scope.row.roleName }}</el-tag>
-          </template>
-        </el-popover>
+        <el-tag :type="scope.row.roleId===1?'success':'warning'">{{ scope.row.roleId }}</el-tag>
       </template>
-    </el-table-column>
-    <el-table-column label="编辑">
-      <template #default="scope">
-        <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
-          >编辑</el-button
-        >
-        <el-button
-          size="small"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)"
-          >删除</el-button
-        >
-      </template>
-    </el-table-column>
-  </el-table>
+      </el-table-column>
+      <el-table-column label="角色名称" align="center">
+        <template #default="scope">
+          <el-tag :type="scope.row.roleId===1?'danger':''">{{ scope.row.roleName }}</el-tag>
+        </template>
+      </el-table-column>
+    </template>
+  </GeneralTables>
 </template>
 
 <style scoped lang="scss">
